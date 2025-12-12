ec542d974439f3176e27fa1e4619ed6afb5a5fc0
SadhviNarayanan <sadhvin@gmail.com>
2025-12-12 11:04:59 -0800

    exceptionsH coverpoints running

--- framework/src/act/fcov/coverage/RISCV_coverage_config.svh
+++ framework/src/act/fcov/coverage/RISCV_coverage_config.svh
@@ -254,6 +254,9 @@
 `ifdef EXCEPTIONSHV_COVERAGE
   `include "ExceptionsHV_coverage.svh"
 `endif
+`ifdef EXCEPTIONSH_COVERAGE
+  `include "ExceptionsH_coverage.svh"
+`endif
 `ifdef EXCEPTIONSVF_COVERAGE
   `include "ExceptionsVF_coverage.svh"
 `endif
